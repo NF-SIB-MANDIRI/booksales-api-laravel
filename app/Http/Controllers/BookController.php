1866639ddbcdd961a8ff4eb3ed92eb2b05969da9
riliapuw <rinaapril1504@gmail.com>
2025-05-21 01:05:46 +0700

    Upload tugas 4

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
     public function index(){
         $books = Book::all();
         
+        if ($books->isEmpty()){
+            return response()->json([
+                "success" => true,
+                "message" => "Resource data not found!"
+            ], 200);
+        }
+
         return response()->json([
             "success" => true,
             "message" => "Get All Resource",
@@ -17,4 +25,42 @@ class BookController extends Controller
         ], 200);
     }
 
+    public function store(Request $request){
+        // 1. Validator
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:100',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+            'cover_photo' => 'required|image|mimes:jpeg,jpg,png|max:2048'
+        ]);
+
+        // 2. Check Validator eror
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+        
+        // 3. Upload Image
+        $image = $request->file('cover_photo');
+        $image->store('books', 'public');
+        
+        // 4. Insert data
+        $book = Book::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock,
+            'cover_photo' => $image->hashName()
+        ]);
+        
+        // 5. Response
+        return response()->json([
+            "success" => true,
+            "message" => "Resource added successfully",
+            "data" => $book
+        ]);
+    }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -2,18 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class GenreController extends Controller
 {
     public function index(){
         $genres = Genre::all();
         
+        if ($genres->isEmpty()){
+            return response()->json([
+                "success" => true,
+                "message" => "Resource data not found!"
+            ], 200);
+        }
+
         return response()->json([
             "success" => true,
             "message" => "Get All Resource",
             "data" => $genres
         ], 200);
+    }
+
+    public function store(Request $request){
+        // 1. Validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string'
+        ]);
+
+        // 2. Check Validator eror
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+        
+        // 3. Insert data
+        $genre = Genre::create([
+            'name' => $request->name,
+            'description' => $request->description
+        ]);
+        
+        // 4. Response
+        return response()->json([
+            "success" => true,
+            "message" => "Resource added successfully",
+            "data" => $genre
+        ]);
     }
 }

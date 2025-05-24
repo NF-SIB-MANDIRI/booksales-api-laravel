ad5ab44c4c24f28c2073b1f13d4945c75e7a69be
riliapuw <rinaapril1504@gmail.com>
2025-05-24 23:20:41 +0700

    Upload tugas 6

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
+use App\Models\Genre;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,9 +12,18 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
-Route::apiResource('/books', BookController::class);
+Route::apiResource('/books', BookController::class)->only(['index', 'show']);
+Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
+Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
 
-Route::apiResource('/genres', GenreController::class);
-
-Route::apiResource('/authors', AuthorController::class);
+Route::middleware('auth:api')->group(function () {
+    Route::middleware(['role:admin'])->group(function () {  
+        Route::apiResource('/books', BookController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/genres', GenreController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/Authors', AuthorController::class)->only(['store', 'update', 'destroy']);
+    });
+});
